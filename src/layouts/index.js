e014308f03f2d5fa031ab0b0ddb8b08c165a1821
Daniel Stockham <ds321609@gmail.com>
2018-04-24 07:52:57 -0400

    manually injected styles on main layout and deleted unused files

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -48,6 +48,9 @@ const TemplateWrapper = ({data, children}) => {
   const nextShow = data.allContentfulShow.edges[0];
   return (
     <div>
+      <Helmet>
+        <link rel="stylesheet" href="styles.css"/>
+      </Helmet>
       <div className="c-background-image"></div>
       <div className="c-background-overlay"></div>
       <div className="wrapper">
