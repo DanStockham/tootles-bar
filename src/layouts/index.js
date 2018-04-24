80aa2efad1038e37a4e70e61b9fca73279d7ae8a
Daniel Stockham <ds321609@gmail.com>
2018-04-24 18:19:54 -0400

    added styles again

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -61,6 +61,7 @@ const TemplateWrapper = ({data, children}) => {
             ]}
             >
             <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"/>
+            <link rel="stylesheet" href="style.css"/>
           </Helmet>
 
           <div className="c-layout__content">
