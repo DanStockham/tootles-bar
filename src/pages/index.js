9751e706282cfc36bb8d5a3ff30a37fd0457f1c6
Daniel Stockham <ds321609@gmail.com>
2018-04-24 19:45:57 -0400

    test index

---
+++ src/pages/index.js
@@ -0,0 +1,18 @@
+import React, { Component, PropTypes } from 'react';
+
+
+import 'bootstrap/dist/css/bootstrap.css';
+import '../styles/index.scss';
+import '../styles/layout.scss';
+
+/**
+ * UpcomingEvents page
+ */
+const IndexPage = ({pathContext}) => { // eslint-disable-line react/prefer-stateless-function
+
+  return (
+    <div />
+  );
+}
+
+export default IndexPage;
